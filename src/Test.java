111841e3b66f8dbdb36922ed9f75c56f9dade8ea
abc <abc@abc-PC>
2019-07-02 11:23:59 +0800

    鞍山市

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+	public static void main(String[] args) {
+		aa();
+	}
+
+	private static void aa() {
+		// TODO Auto-generated method stub
+		bb();
+	}
+
+	private static void bb() {
+		// TODO Auto-generated method stub
+		cc();
+	}
+
+	private static void cc() {
+		// TODO Auto-generated method stub
+		int a=0;
+		int b=1/a;
+	}
+	
+}
